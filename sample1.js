2accdde84ecc424fd2a92e405b2bf348566e3148
angel-sam14 <angelannasam14@gmail.com>
2026-07-23 10:05:51 -0700

    sample1.html

---
+++ sample1.js
@@ -0,0 +1,23 @@
+// alert('Hai from external js');
+// console.log('Hello world'); //type of js
+//  var a = 10
+//  var a = 10//var
+// let a =10
+//  a = 20
+// const a = 10
+//  console.log(`the value of the variable a is: ${a}`)
+
+//  function sample(){
+    // var b = 30
+    // let b = 30
+//     const b = 30
+// }
+// console.log(b)
+
+/*  hoisting*/
+// console.log(a)
+// let a = 10
+
+var a = 10
+console.log(a)
+a = 10
