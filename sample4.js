2da9d437820d113cccda5921ddf7d2c965d06dd8
angel-sam14 <angelannasam14@gmail.com>
2026-07-29 01:30:49 -0700

    sample4.html

---
+++ sample4.js
@@ -0,0 +1,97 @@
+// // switch
+
+// day
+
+// let day = 5
+// let dayName
+//       switch(day){
+//           case 1:
+//             dayName = "Sunday"
+//         break
+
+//         case 2:
+//             dayName = "Monday"
+//         break
+
+//         case 3:
+//             dayName = "Tuesday"  
+//         break
+
+//         case 4:
+//             dayName = "Wednesday"  
+//         break
+
+//         case 5:
+//             dayName = "Thursday"  
+//         break
+
+//         case 6:
+//             dayName = "Friday"  
+//         break
+
+//         case 7:
+//             dayName = "Saturday"  
+//         break
+
+//         default:
+//             dayName = "Invalid day"
+    
+// }
+// console.log(dayName)
+
+
+// grade
+
+
+// let mark = 85;
+// let grade;
+
+// switch (true) {
+//     case mark >= 90:
+//         grade = "A+";
+//         break;
+
+//     case mark >= 80:
+//         grade = "A";
+//         break;
+
+//     case mark >= 70:
+//         grade = "B+";
+//         break;
+
+//     case mark >= 60:
+//         grade = "B";
+//         break;
+
+//     case mark >= 50:
+//         grade = "C";
+//         break;
+
+//     default:
+//         grade = "Fail";
+// }
+
+// console.log(grade);
+
+
+// loop
+
+// for (intialization;condition;increment/decrement){
+// code block
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i%2===0)
+//     console.log(i)
+// }
+
+
+// number
+
+
+let firstNumber = Number(prompt("Enter the first number:"))
+let SecondNumber = Number(prompt("Enter the Second number:"))
+
+for(let i=firstNumber;i<=SecondNumber;i++){
+    console.log(i)
+}
